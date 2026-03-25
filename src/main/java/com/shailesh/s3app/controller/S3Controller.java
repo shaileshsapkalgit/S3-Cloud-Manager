757009df2d12b4b79c8f89864864e32984cd799a
Shailesh Sapkal <shaileshsapkal1829@gmail.com>
2026-03-25 13:52:37 +0530

    updated index.html

--- src/main/java/com/shailesh/s3app/controller/S3Controller.java
+++ src/main/java/com/shailesh/s3app/controller/S3Controller.java
@@ -2,7 +2,12 @@ package com.shailesh.s3app.controller;
 
 import com.shailesh.s3app.service.S3Service;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/files")
@@ -11,17 +16,24 @@ public class S3Controller {
     private S3Service s3Service;
 
     @PostMapping("/upload")
-    public String upload(@RequestParam String name, @RequestBody byte[] content) {
-        return s3Service.uploadFile(name, content);
+    public String upload(@RequestParam("file") MultipartFile file) throws IOException {
+        return s3Service.uploadFile(file.getOriginalFilename(), file.getBytes());
     }
 
     @GetMapping("/download/{name}")
-    public byte[] download(@PathVariable String name) {
-        return s3Service.downloadFile(name);
+    public ResponseEntity<byte[]> download(@PathVariable String name) {
+        byte[] data = s3Service.downloadFile(name);
+        return ResponseEntity.ok()
+                .header("Content-Disposition", "attachment; filename=\"" + name + "\"")
+                .body(data);
     }
 
     @DeleteMapping("/delete/{name}")
     public String delete(@PathVariable String name) {
         return s3Service.deleteFile(name);
+    }
+    @GetMapping("/all")
+    public List<String> getAllFiles() {
+        return s3Service.listFiles();
     }
 }
