695810c28f6a38caafbfda76c880c12c3c4e69cc
Shailesh Sapkal <shaileshsapkal1829@gmail.com>
2026-04-14 23:35:30 +0530

    Infrastructure setup complete in us-west-1. Deploying Spring Boot S3 app.

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -2,18 +2,13 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.0.0"
 
-  name = "shailesh-vpc"
-  cidr = "10.0.0.0/16"
+  name = "full-devops-vpc"
+  cidr = "10.20.0.0/16"
 
-  # ✅ us-east-1 साठी परफेक्ट झोन्स
-  azs             = ["us-east-1a", "us-east-1b"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+  azs             = ["eu-north-1a", "eu-north-1b", "eu-north-1c"]
+  private_subnets = ["10.20.1.0/24", "10.20.2.0/24"]
+  public_subnets  = ["10.20.101.0/24", "10.20.102.0/24"]
 
   enable_nat_gateway = true
   single_nat_gateway = true
-
-  public_subnet_tags = {
-    "kubernetes.io/role/elb" = "1"
-  }
 }
