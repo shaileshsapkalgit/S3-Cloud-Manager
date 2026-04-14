621e2ce62a1cd250ae61410aaa07eb10c0cc2e32
Shailesh Sapkal <shaileshsapkal1829@gmail.com>
2026-04-14 13:56:25 +0530

    End-to-end automation with Terraform and Helm

---
+++ terraform/helm.tf
@@ -0,0 +1,28 @@
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+      command     = "aws"
+    }
+  }
+}
+
+resource "helm_release" "bank_app" {
+  name       = "shailesh-bank-app"
+  chart      = "./my-bank-app" # इथे तुझ्या हेल्म चार्ट फोल्डरचा पाथ दे
+
+  set {
+    name  = "service.port"
+    value = "8080"
+  }
+
+  set {
+    name  = "service.targetPort"
+    value = "8080"
+  }
+
+  depends_on = [module.eks]
+}
