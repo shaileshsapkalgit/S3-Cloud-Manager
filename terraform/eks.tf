f36483d84c715f1013703e0cbdb7e6a1e71a3c25
Shailesh Sapkal <shaileshsapkal1829@gmail.com>
2026-04-14 23:37:52 +0530

    Migrated infrastructure to us-east-1 to bypass VPC limits

---
+++ terraform/eks.tf
@@ -0,0 +1,20 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "19.15.3"
+
+  cluster_name    = "shailesh-s3-cluster"
+  cluster_version = "1.31"
+
+  vpc_id                         = module.vpc.vpc_id
+  subnet_ids                     = module.vpc.private_subnets
+  cluster_endpoint_public_access = true
+
+  eks_managed_node_groups = {
+    nodes = {
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
+      instance_types = ["t3.small"]
+    }
+  }
+}
--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -5,7 +5,7 @@ module "vpc" {
   name = "full-devops-vpc"
   cidr = "10.20.0.0/16"
 
-  azs             = ["eu-north-1a", "eu-north-1b", "eu-north-1c"]
+  azs = ["us-west-1a", "us-west-1c"]
   private_subnets = ["10.20.1.0/24", "10.20.2.0/24"]
   public_subnets  = ["10.20.101.0/24", "10.20.102.0/24"]
 
