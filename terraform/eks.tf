6f2cf933c5fee064bb43fad711743211071af49a
Shailesh Sapkal <shaileshsapkal1829@gmail.com>
2026-04-14 14:10:41 +0530

    deployment.yaml file push

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -2,7 +2,7 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "19.15.3"
 
-  cluster_name    = "shailesh-terraform-cluster"
+  cluster_name    = "shailesh-terraform-cluster-new"
   cluster_version = "1.31"
 
   vpc_id                         = module.vpc.vpc_id
